24335a60585c1f5dda4f5be0980c444c88a0dc23
oscar <31993667+monzonoi@users.noreply.github.com>
2023-09-13 18:59:04 -0300

    agrego proyecto web con login useridentity
    agrego proyecto web con login useridentity

--- Cafeteria.WEB/Data/WeatherForecast.cs
+++ Cafeteria.WEB/Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace Cafeteria.WEB.Data
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
--- Cafeteria.WEB/Program.cs
+++ Cafeteria.WEB/Program.cs
@@ -1,47 +1,34 @@
-using Cafeteria.Intraestructura;
+using Cafeteria.WEB.Areas.Identity;
 using Cafeteria.WEB.Data;
-using Cafeteria.WEB.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
-//builder.Services.AddScoped<ApiService>();
-
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
-
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Personaliza las opciones de Identity según tus necesidades
-    options.SignIn.RequireConfirmedAccount = true;
-});
-
-
-builder.Services.AddAuthentication()
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Identity/Account/Login"; // Ruta de inicio de sesión
-    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -54,11 +41,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
